912dc46d11368cd63667ea056d484525698771e5
Nicolas Martinez <fyedream@gmail.com>
2019-07-14 06:22:35 -0600

    Modified NavBar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from "./components/NavBar";
-import Counter from "./components/Counter"
+
 import "./app.css";
 
 
@@ -8,11 +8,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <div className="test-div">
-        <Counter props={{ title: "test title 1", number: "8" }} />
-        <Counter props={{ title: "test title 2", number: "10" }} />
-        <Counter props={{ title: "test title 3", number: "12" }} />
-      </div>
+     
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import Counter from "./Counter";
 import "./navbar.css"
 
 const NavBar = () => {
-    return ( 
-       <header>
-           Dashboard
-       </header>
-     );
+    return (
+        <>
+            <header> Dashboard </header>
+            <span>ggggggg/ggggggggg</span>
+            <div className="main-container">
+
+                <div className="user-salute-container">
+                    <img className="profile-img" src="https://vignette1.wikia.nocookie.net/sote-rp/images/c/c4/User-placeholder.png/revision/latest?cb=20150624004222" alt="Img here"></img>
+                    <div className="user-salute-sub-container">
+                        <span className="main-salute">Good morning, Serati Ma, i wish you happiness every day!</span>
+                        <span className="user-background">
+                            Interaction expert | Ant gold service-some certain business
+                        </span>
+                    </div>
+                </div>
+                <div className="counters-container">
+                    <Counter props={{ title: "Number of items", number: "56" }} />
+                    <div className="divider" />
+                    <Counter props={{ title: "Teams ranking", number: "8" }} />
+                    <div className="divider" />
+                    <Counter props={{ title: "Project visits", number: "2,223" }} />
+                </div>
+            </div>
+        </>
+    );
 }
- 
+
 export default NavBar;
