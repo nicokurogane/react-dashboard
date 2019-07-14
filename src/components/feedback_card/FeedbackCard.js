81345b91c29e248c0752b587269582ebcc3f2326
Nicolas Martinez <fyedream@gmail.com>
2019-07-14 15:51:31 -0600

    Update dependencies and feedback list and card components .

--- src/components/feedback_card/FeedbackCard.js
+++ src/components/feedback_card/FeedbackCard.js
@@ -7,8 +7,8 @@ const FeedbackCard = () => {
             <img src ="https://vignette1.wikia.nocookie.net/sote-rp/images/c/c4/User-placeholder.png/revision/latest?cb=20150624004222" 
             className="feedback-profile_img" alt="profile"></img>
             <div className="feedback-info-container">
-                <span>stars of the week in high force grid-day mission, the design of new projects iteration june</span>
-                <span>2days ago</span>
+                <span className="feedback-info">stars of the week in high force grid-day mission, the design of new projects iteration june</span>
+                <span className="feedback-status-time">2days ago</span>
             </div>
         </div>
      );
--- src/components/feedback_list/FeedbackList.js
+++ src/components/feedback_list/FeedbackList.js
@@ -4,16 +4,21 @@ import FeedbackCard from '../feedback_card/FeedbackCard';
 import './feedbacklist.css';
 
 const FeedbackList = () => {
-    return ( 
+    return (
         <div className="feedback-list-container">
             <div className="feedback-header-container">
                 <span>Lastest Events</span>
             </div>
             <div className="projects-container">
-                <FeedbackCard/>
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard />
+                <FeedbackCard /> 
+                <FeedbackCard />
+                <FeedbackCard />
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default FeedbackList;
