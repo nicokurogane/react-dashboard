9f1459460f1f77fbd9935e2dd76744db38f8821e
nicokurogane <fyedream@gmail.com>
2019-07-15 17:14:34 -0600

    Added dividers to chart legends

--- src/components/graphic_card/GraphicCard.js
+++ src/components/graphic_card/GraphicCard.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import ChartLegend from '../chart_legend/ChartLegend';
+import React from "react";
+import ChartLegend from "../chart_legend/ChartLegend";
 
-import './graphiccard.css';
+import "./graphiccard.css";
 
-import radarChart from './radar-charts.jpg';
-
-
+import radarChart from "./radar-charts.jpg";
 
 const GraphinCard = () => {
-    return (
-        <div className="graphic-card-container">
-            <div className="graphic-card-header">
-                <span>XX index</span>
-            </div>
-            <div className="graphic-card-info-container">
-                <img src={radarChart} alt="radar chart" className="radar-chart"/>
-                <div className="chart-scores-container">
-                <ChartLegend props={{ title: "personal", number: "34", color: "#4263f5" }} />
-                <ChartLegend props={{ title: "team", number: "22", color: "#b0240e"  }} />
-                <ChartLegend props={{ title: "department", number: "23", color: "#d6c90d"  }} />
-                </div>
-            </div>
+  return (
+    <div className="graphic-card-container">
+      <div className="graphic-card-header">
+        <span>XX index</span>
+      </div>
+      <div className="graphic-card-info-container">
+        <img src={radarChart} alt="radar chart" className="radar-chart" />
+        <div className="chart-scores-container">
+          <ChartLegend
+            props={{ title: "personal", number: "34", color: "#4263f5" }}
+          />
+          <div className="vbar-legend" />
+          <ChartLegend
+            props={{ title: "team", number: "22", color: "#b0240e" }}
+          />
+          <div className="vbar-legend" />
+          <ChartLegend
+            props={{ title: "department", number: "23", color: "#d6c90d" }}
+          />
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default GraphinCard;
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,37 +1,51 @@
-import React from 'react';
+import React from "react";
 import Counter from "../counter/Counter";
-import "./navbar.css"
+import "./navbar.css";
 
 const NavBar = () => {
-    return (
-        <>
-            <header> Dashboard </header>
-            <div className="header-container">
-            <div className="breadcumb-container">
-                <span>Home / Dashboard / Workplace</span>
+  return (
+    <>
+      <header> Dashboard </header>
+      <div className="header-container">
+        <div className="breadcumb-container">
+          <span>Home / Dashboard / Workplace</span>
+        </div>
+        <div className="main-container">
+          <div className="user-salute-container">
+            <img
+              className="profile-img"
+              src="https://vignette1.wikia.nocookie.net/sote-rp/images/c/c4/User-placeholder.png/revision/latest?cb=20150624004222"
+              alt="Img here"
+            />
+            <div className="user-salute-sub-container">
+              <span className="main-salute">
+                Good morning, Serati Ma, i wish you happiness every day!
+              </span>
+              <span className="user-background">
+                Interaction expert | Ant gold service-some certain business
+              </span>
             </div>
-            <div className="main-container">
-
-                <div className="user-salute-container">
-                    <img className="profile-img" src="https://vignette1.wikia.nocookie.net/sote-rp/images/c/c4/User-placeholder.png/revision/latest?cb=20150624004222" alt="Img here"></img>
-                    <div className="user-salute-sub-container">
-                        <span className="main-salute">Good morning, Serati Ma, i wish you happiness every day!</span>
-                        <span className="user-background">
-                            Interaction expert | Ant gold service-some certain business
-                        </span>
-                    </div>
-                </div>
-                <div className="counters-container">
-                    <Counter className="counter-component" props={{ title: "Number of items", number: "56" }} />
-            
-                    <Counter className="counter-component" props={{ title: "Teams ranking", number: "8" }} />
-         
-                    <Counter className="counter-component" props={{ title: "Project visits", number: "2,223" }} />
-                </div>
-            </div>
-            </div>
-        </>
-    );
-}
+          </div>
+          <div className="counters-container">
+            <Counter
+              className="counter-component"
+              props={{ title: "Number of items", number: "56" }}
+            />
+            <div className="vbar" />
+            <Counter
+              className="counter-component"
+              props={{ title: "Teams ranking", number: "8" }}
+            />
+            <div className="vbar" />
+            <Counter
+              className="counter-component"
+              props={{ title: "Project visits", number: "2,223" }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default NavBar;
