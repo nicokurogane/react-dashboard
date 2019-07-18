fa2911ad1d58198c4320047c8df5664147338609
Nicolas Martinez <fyedream@gmail.com>
2019-07-17 21:54:23 -0600

    Added some minimal changes

--- src/App.js
+++ src/App.js
@@ -34,11 +34,11 @@ function App() {
     ),
     new Event(
       2,
-      "https://vignette1.wikia.nocookie.net/sote-rp/images/c/c4/User-placeholder.png/revision/latest?cb=20150624004222",
-      "in",
-      "of new projects",
-      "high force grid day mission, the design",
-      "interation June",
+      "https://vignette.wikia.nocookie.net/es.starwars/images/a/ac/Republic_Emblem_%28unification_wars%29.svg/revision/latest?cb=20081202012549",
+      "The empire will",
+      "of the",
+      "start desintegration",
+      "Rebel Scum",
       "2 weeks ago"
     ),
     new Event(
@@ -62,10 +62,10 @@ function App() {
     new Event(
       5,
       "https://vignette1.wikia.nocookie.net/sote-rp/images/c/c4/User-placeholder.png/revision/latest?cb=20150624004222",
-      "in",
-      "of new projects",
-      "high force grid day mission, the design",
+      "My god! will",
       "",
+      "throw some heat before evening,",
+      "so get ready",
       "a year ago"
     )
   ];
@@ -76,48 +76,48 @@ function App() {
       team_logo,
       "bootstrap",
       "Hope is a good thing, maybe the best, good things will not die",
-      "the whole group",
-      "2 years ago"
+      "the boostrap group",
+      "2 hours ago"
     ),
     new Project(
       2,
       "https://cdn2.iconfinder.com/data/icons/designer-skills/128/react-512.png",
       "React",
       "Hope is a good thing, maybe the best, good things will not die",
-      "the whole group",
+      "the react gang",
       "2 years ago"
     ),
     new Project(
       3,
-      team_logo,
-      "bootstrap",
-      "Hope is a good thing, maybe the best, good things will not die",
-      "the whole group",
-      "2 years ago"
+      "https://cdn.worldvectorlogo.com/logos/angular-icon.svg",
+      "angular",
+      "Heroes never die!, so try to remember them everyday of the year!",
+      "Angular Dev Team",
+      "Few minutes ago"
     ),
     new Project(
       4,
-      team_logo,
-      "bootstrap",
-      "Hope is a good thing, maybe the best, good things will not die",
-      "the whole group",
+      "https://lh3.googleusercontent.com/xV6gK4t0SF7AW4LK3slUzj4WIY1SfzRtq-zFTygFSO9wg-JXTJImEi34rrxhsRutXQ=s180",
+      "Alipay",
+      "Write here the gangstest song you've ever heard (P.I.M.P -  50 Cents)",
+      "the familiy kingdom",
       "2 years ago"
     ),
     new Project(
       5,
-      team_logo,
-      "bootstrap",
+      "https://camo.githubusercontent.com/a717488dab48dea50d4d2fb3fe8977b0a4f29147/68747470733a2f2f7a6f732e616c697061796f626a656374732e636f6d2f726d73706f7274616c2f77496a4d446e7372446f50506349562e706e67",
+      "Ant design",
       "Hope is a good thing, maybe the best, good things will not die",
       "the whole group",
-      "2 years ago"
+      "7 years ago"
     ),
     new Project(
       6,
-      team_logo,
-      "bootstrap",
+      "https://cdn.worldvectorlogo.com/logos/nodejs-icon.svg",
+      "nodeJS",
       "Hope is a good thing, maybe the best, good things will not die",
       "the whole group",
-      "2 years ago"
+      "3 weeks ago"
     )
   ];
 
